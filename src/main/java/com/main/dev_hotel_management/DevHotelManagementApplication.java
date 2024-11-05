6f39b9194b957961a5fd61b138277283269bc513
[Debmalya Pan] <[debmalyapan4@gmail.com]>
2024-11-05 21:26:39 +0530

    addfiles

---
+++ src/main/java/com/main/dev_hotel_management/DevHotelManagementApplication.java
@@ -0,0 +1,13 @@
+package com.main.dev_hotel_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevHotelManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevHotelManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/main/dev_hotel_management/DevHotelManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.main.dev_hotel_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevHotelManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
